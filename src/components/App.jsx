3b09c133ebd31d438b84a92af50993e7ece7a2b1
Heldenglanz <heldenglanz@gmail.com>
2022-09-08 19:25:26 +0300

    fff

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: 'red'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <div>ffff</div>
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101'
+        }}
+      >
+        React homework template
+      </div>
+    </>
   );
 };
